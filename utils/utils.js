48293d9c08a4423f28e27cf0050ee8b87f35ae1b
Moeed Bhatti <moeedbhatti419@gmail.com>
2022-07-06 12:30:41 +0500

    change port for heroku

--- utils/utils.js
+++ utils/utils.js
@@ -1,4 +1,4 @@
 const mongodb = "mongodb+srv://moeedbhati77:comsian77@fyp.j8xo4.mongodb.net/";
 const secret = "secret123";
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 module.exports = { secret, mongodb, port };
